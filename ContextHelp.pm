3d402646fb34c940ac2ee71e5beed653ff716d1c
Slaven Rezic <slaven@rezic.de>
1998-02-18 00:05:21 +0000

    states ConfigSpecs funktioniert anscheinend ordentlich

--- ContextHelp.pm
+++ ContextHelp.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: ContextHelp.pm,v 1.1 1998/02/17 18:06:44 eserte Exp $
+# $Id: ContextHelp.pm,v 1.2 1998/02/18 00:05:21 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 1998 Slaven Rezic. All rights reserved.
@@ -27,45 +27,77 @@ sub Populate {
 
     $w->overrideredirect(1);
     $w->withdraw;
-    $w->bind('<Button-1>' => sub { $w->withdraw });
+    $w->bind('<Button-1>' => [ $w, 'deactivate']);
+    $w->bind('<Button-2>' => [ $w, 'deactivate']);
+    $w->bind('<Button-3>' => [ $w, 'deactivate']);
+    $w->bind('<Key>'      => [ $w, 'deactivate']); # XXX geht nicht?
 
-    $w->{'label'} = $w->Label->pack;
+    my $widget = delete $args->{'-widget'} || 'Label';
+    $w->{'label'} = $w->$widget()->pack;
+    $w->{'state'} = 'withdrawn';
+
+    $w->ConfigSpecs
+      (-installcolormap => ["PASSIVE", "installColormap", "InstallColormap", 0],
+       -background      => [$w->{'label'}, "background", "Background", "#C0C080"],
+       -font            => [$w->{'label'}, "font", "Font", "-*-helvetica-medium-r-normal--*-120-*-*-*-*-*-*"],
+       -borderwidth     => ["SELF", "borderWidth", "BorderWidth", 1],
+       DEFAULT          => [$w->{'label'}],
+      );
 }
 
 sub activate {
-    my($w) = @_;
+    my($w, $state) = @_;
+    $state = 'context' unless $state;
+    $w->deactivate unless $state eq 'wait';
     my $top = $w->parent->toplevel;
     my $inp_only = $top->InputO
       (-width  => $top->width,
        -height => $top->height,
-       -cursor => ['@' . Tk->findINC('context_help.xbm'), 'black'],
-      )->place(-x => 0, -y => 0);
+      )->place(-x => 0, -y => 0, -relwidth => 1.0, -relheight => 1.0);
+    if ($state eq 'context') {
+	$inp_only->configure
+	  (-cursor => ['@' . Tk->findINC('context_help.xbm'), 'black']);
+    }
     $w->{'inp_only'} = $inp_only;
+    $w->{'state'} = $state;
     $inp_only->bind('<Button-1>' => sub {
-			my $e = $_[0]->XEvent;
-			my($x, $y) = ($e->x, $e->y);
-			$w->deactivate;
-			my($rootx, $rooty) = ($x+$top->rootx, $y+$top->rooty);
-			my $under = $top->containing($rootx, $rooty);
-			if (!defined $under ||
-			    !exists $w->{'msg'}{$under}) {
+			if ($w->{'state'} eq 'context') {
+			    my $e = $_[0]->XEvent;
+			    my($x, $y) = ($e->x, $e->y);
+			    $w->deactivate;
+			    my($rootx, $rooty) = ($x+$top->rootx,
+						  $y+$top->rooty);
+			    my $under = $top->containing($rootx, $rooty);
+			    # underlying widget and its parents
+			    while(defined $under) {
+				if (exists $w->{'msg'}{$under}) {
+				    if ($w->cget(-installcolormap)) {
+					$w->colormapwindows($top);
+				    }
+				    $w->{'label'}->configure
+				      (-text => $w->{'msg'}{$under});
+				    $w->geometry("+$rootx+$rooty");
+				    $w->deiconify;
+				    $w->raise;
+				    $w->update;
+				    $w->activate('wait');
+				    return;
+				}
+				$under = $under->parent;
+			    }
 			    $top->bell;
-			} else {
-			    $w->{'label'}->configure
-			      (-text => $w->{'msg'}{$under});
-			    $w->geometry("+$rootx+$rooty");
-			    $w->deiconify;
-			    $w->raise;
-			    $w->update;
 			}
+			$w->deactivate;
 		    });
-    $inp_only->bind('<Button-2>' => [$w, 'deactivate']);
-    $inp_only->bind('<Button-3>' => [$w, 'deactivate']);
-    $inp_only->bind('<Key>'  => [$w, 'deactivate']);
+    $inp_only->bind('<Button-2>'  => [$w, 'deactivate']);
+    $inp_only->bind('<Button-3>'  => [$w, 'deactivate']);
+    $inp_only->bind('<Key>'       => [$w, 'deactivate']); # XXX geht nicht?
 }
 
 sub deactivate {
     my($w) = @_;
+    $w->withdraw;
+    $w->{'state'} = 'withdrawn';
     if (Tk::Exists($w->{'inp_only'})) {
 	$w->{'inp_only'}->destroy;
 	delete $w->{'inp_only'};
@@ -83,7 +115,7 @@ sub detach {
     delete $w->{'msg'}{$client};
 }
 
-sub CHButton {
+sub HelpButton {
     my($w, $top, %args) = @_;
     $args{-bitmap} = '@' . Tk->findINC('context_help.xbm')
       unless $args{-bitmap};
@@ -93,24 +125,23 @@ sub CHButton {
 
 1;
 __END__
-# Below is the stub of documentation for your module. You better edit it!
 
 =head1 NAME
 
-Tk::ContextHelp - Perl extension for blah blah blah
+Tk::ContextHelp - context-sensitive help with perl/Tk
 
 =head1 SYNOPSIS
 
   use Tk::ContextHelp;
-  blah blah blah
+
+  $ch = $top->ContextHelp;
+  $ch->attach($widget, -msg => ...);
+
+  $top->HelpButton->pack;
 
 =head1 DESCRIPTION
 
-Stub documentation for Tk::ContextHelp was created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+XXX
 
 =head1 AUTHOR
 
@@ -118,6 +149,6 @@ Slaven Rezic <eserte@cs.tu-berlin.de>
 
 =head1 SEE ALSO
 
-perl(1).
+Tk::Balloon(3).
 
 =cut
